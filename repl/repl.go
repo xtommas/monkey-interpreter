37b40516e48432efc1ec90dd7f51a69fbb0abceb
tomas <tomascanepa1397@gmail.com>
2023-09-15 12:32:15 -0300

    added evaluation to the repl (Read Eval Print Loop)

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/xtommas/monkey-interpreter/evaluator"
 	"github.com/xtommas/monkey-interpreter/lexer"
 	"github.com/xtommas/monkey-interpreter/parser"
 )
@@ -43,8 +44,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
