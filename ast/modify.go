2fc1b0e9b4ecbb355b0f2b381a682b9a20dbc75d
tomas <tomascanepa1397@gmail.com>
2023-09-21 12:27:46 -0300

    walking AST: infix expressions

---
+++ ast/modify.go
@@ -0,0 +1,21 @@
+package ast
+
+type ModifierFunc func(Node) Node
+
+func Modify(node Node, modifier ModifierFunc) Node {
+	switch node := node.(type) {
+	case *Program:
+		for i, statement := range node.Statements {
+			node.Statements[i], _ = Modify(statement, modifier).(Statement)
+		}
+
+	case *ExpressionStatement:
+		node.Expression, _ = Modify(node.Expression, modifier).(Expression)
+
+	case *InfixExpression:
+		node.Left, _ = Modify(node.Left, modifier).(Expression)
+		node.Right, _ = Modify(node.Right, modifier).(Expression)
+	}
+
+	return modifier(node)
+}
