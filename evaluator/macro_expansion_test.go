61539517cf45d3ae134300638e79757d6b8f3e76
tomas <tomascanepa1397@gmail.com>
2023-09-22 14:10:28 -0300

    macros support

--- evaluator/macro_expansion_test.go
+++ evaluator/macro_expansion_test.go
@@ -53,8 +53,55 @@ func TestDefineMacros(t *testing.T) {
 		t.Fatalf("body is not %q. got=%q", expectedBody, macro.Body.String())
 	}
 }
+
 func testParseProgram(input string) *ast.Program {
 	l := lexer.New(input)
 	p := parser.New(l)
 	return p.ParseProgram()
+}
+
+func TestExpandMacros(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{
+			`
+	let infixExpression = macro() { quote(1 + 2); };
+	infixExpression();
+	`,
+			`(1 + 2)`,
+		},
+		{
+			`
+	let reverse = macro(a, b) { quote(unquote(b) - unquote(a)); };
+	reverse(2 + 2, 10 - 5);
+	`,
+			`(10 - 5) - (2 + 2)`,
+		},
+		{
+			`
+			let unless = macro(condition, consequence, alternative) {
+			quote(if (!(unquote(condition))) {
+			unquote(consequence);
+			} else {
+			unquote(alternative);
+			});
+			};
+			unless(10 > 5, puts("not greater"), puts("greater"));
+			`,
+			`if (!(10 > 5)) { puts("not greater") } else { puts("greater") }`,
+		},
+	}
+	for _, tt := range tests {
+		expected := testParseProgram(tt.expected)
+		program := testParseProgram(tt.input)
+		env := object.NewEnvironment()
+		DefineMacros(program, env)
+		expanded := ExpandMacros(program, env)
+		if expanded.String() != expected.String() {
+			t.Errorf("not equal. want=%q, got=%q",
+				expected.String(), expanded.String())
+		}
+	}
 }
